16046a1ef0f03ec8f0a12ada4e300041b2dee1a0
William Rao Fernandes <williamraofernandes@gmail.com>
2023-01-10 10:02:22 +0100

    Update server.js

--- semaine_02_approfondissement/J2_middleware/Corrections_exercices/01_logger/server.js
+++ semaine_02_approfondissement/J2_middleware/Corrections_exercices/01_logger/server.js
@@ -1,13 +1,24 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import {incrementCompteur, logger} from "middlewares.js";
+import { logger, requestCounter } from "./middlewares.js";
 
+// ==========
+// App initialization
+// ==========
+
+const hostname = 'localhost';
 const port = 8000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.URL));
 
+// ==========
+// App middlewares
+// ==========
 
-app.use(express.urlencoded({extended : false}));
-app.use(incrementCompteur);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(requestCounter);
 app.use(logger);
